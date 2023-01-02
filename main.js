3f6368feb720b396e42285a634462ff7cf58ca91
ralph601 <jeff0101@naver.com>
2023-01-02 15:15:27 +0900

    design update

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let observer = new IntersectionObserver((parm)=>{
-  parm.forEach((box)=>{
-    box.targer.style.opacity = 1;
-  })
-})
+// let observer = new IntersectionObserver((parm)=>{
+//   parm.forEach((box)=>{
+//     box.targer.style.opacity = 1;
+//   })
+// })
 
- let div = document.querySelectorAll('div');
- observer.observe(div[0]);
+//  let div = document.querySelectorAll('div');
+//  observer.observe(div[0]);
